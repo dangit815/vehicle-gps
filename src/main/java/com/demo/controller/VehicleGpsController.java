91ec4eb76e1b271df8a96d64b78c1818f2e78b6d
Wwwwei <zhaosw1995@foxmail.com>
2018-01-12 17:49:20 +0800

    车辆速度监控

--- src/main/java/com/demo/controller/VehicleGpsController.java
+++ src/main/java/com/demo/controller/VehicleGpsController.java
@@ -1,24 +1,18 @@
 package com.demo.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.demo.dao.entity.VehicleGps;
 import com.demo.dto.input.SpeedParam;
 import com.demo.dto.input.TransportParam;
 import com.demo.service.VehicleGpsService;
-import com.demo.utils.BaiduMapUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by wwwwei on 18/1/8.
